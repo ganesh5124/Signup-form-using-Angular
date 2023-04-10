f598c39c6a4b91cd9d1a78ad6cc0c9402ed6b5aa
Pepakayala Veera Ganesh Kumar <pepakayalaveeraganeshkumar@Pepakayalas-MacBook-Air.local>
2023-04-10 13:13:26 +0530

    10th of april commit

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Passwordmatch } from '../validator/passwordmatch';
 
@@ -7,7 +7,7 @@ import { Passwordmatch } from '../validator/passwordmatch';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class signupComponent {
+export class signupComponent implements OnInit {
   authForm = new FormGroup({
     username: new FormControl('',[
       Validators.required,
@@ -26,9 +26,20 @@ export class signupComponent {
       Validators.maxLength(20)
     ])
   },
-  { validators:[this.passwordMatch.validate]}
+  { Validators:[this.passwordMatch.validate]}
   ) 
 
   constructor(public passwordMatch: Passwordmatch){ }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+
+ ngOnChanges(changes: SimpleChanges): void {
+  //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+  //Add '${implements OnChanges}' to the class.
+  
+ }
 }
 
